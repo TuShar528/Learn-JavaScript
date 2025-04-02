6ec47c3dd87425a14cc014cb0b074e0164e0fb50
Tushar <119734788+TuShar528@users.noreply.github.com>
2025-04-02 09:17:29 +0000

    compare

---
+++ 01_basics/05_Comparision.js
@@ -0,0 +1,20 @@
+console.log(2>2)
+console.log(2<2)
+console.log(2==2)
+console.log(2>=2)
+console.log(2<=2)
+// All of these give output in boolean ( in true or false)
+
+console.log("2" == 2)  //output true because it convert string to number
+
+console.log(null > 0)  //output false  
+console.log(null == 0)  //output false
+console.log(null >= 0)  //output true
+
+console.log(undefined > 0)  //output true
+console.log(undefined == 0)  //output true
+console.log(undefined >= 0)  //output true
+
+//strict comparision
+
+console.log("2" === 2)  //gives output as false because it also checks the datatype
