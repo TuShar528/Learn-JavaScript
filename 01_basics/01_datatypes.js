83856ce2e695759b039dd473b4d6ef289038bba0
Tushar <119734788+TuShar528@users.noreply.github.com>
2025-04-02 08:09:20 +0000

    dataypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,8 @@
+let fullname = "Tushar"  // string datatype
+let age = 18             // number datatype
+let LoggedIn = true      // boolean datatype
+
+let state;              // undefined datatype
+let married = null      // object type datatype
+
+console.table([typeof fullname , typeof age , typeof LoggedIn , typeof state , typeof married])
