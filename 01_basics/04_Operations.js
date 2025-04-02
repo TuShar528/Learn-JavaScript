4c32f7414afe67b85bb674b4f061cbb69394616c
Tushar <119734788+TuShar528@users.noreply.github.com>
2025-04-02 08:56:45 +0000

    Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,19 @@
+let score = 33
+console.log( "4" + score )  //Output -> 433
+console.log( 4 + score )    //Output -> 37
+
+let str1="Hello"
+let str2=" Tushar"
+let str3 = str1 + str2
+console.log(str3)           //output -> Hello Tushar
+
+console.log(2+3);       //5
+console.log(2-3);       //-1   
+console.log(2*3);       //6
+console.log(2**3);      //8   (Power)
+console.log(2/3);       //0.6666     (Divide)
+console.log(2%3);       //2          (Modulus - Reminder)
+
+let value = 3
+value = -value
+console.log(value)      // output is -3
