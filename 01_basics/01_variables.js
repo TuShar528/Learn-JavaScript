e203e091dbd7db3a23841295960dd76c1e175bc1
Tushar <119734788+TuShar528@users.noreply.github.com>
2025-04-02 07:48:59 +0000

    container config commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,17 @@
 const id = 12254
-let name = "Tushar Jangid"
+let fullname = "Tushar Jangid"
 var email = "tushar@gmail.com"
+
 console.log(id)
+
+address="india"  // We can also declare variable like this in javascript
+
+//id = 5225  // constant cannot change in js. it will give an error
+email = "ansh@gmail.com"
+fullname = "ansh"
+
+let state;  // it will be assigned as undefined.
+
+console.table([id,fullname,email,address,state])
+
+
